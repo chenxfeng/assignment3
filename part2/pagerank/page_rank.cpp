2472a52c29a111e2679727cdf28a187c24c039cf
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-11-10 22:55:49 +0800

    check score

--- part2/pagerank/page_rank.cpp
+++ part2/pagerank/page_rank.cpp
@@ -90,9 +90,9 @@ void pageRank(DistGraph &g, double* solution, double damping, double convergence
             if (g.world_rank == 0) 
                 printf("%d, %f, %f\n", vi, score_next[vi - g.start_vertex], score_curr[vi]);
             local_diff += abs(score_next[vi - g.start_vertex] - score_curr[vi]);
-            score_curr[vi] = score_next[vi - g.start_vertex];
             if (g.world_rank == 0)
-                printf("%f\n", local_diff);
+                printf("%f, %f\n", local_diff, score_next[vi - g.start_vertex] - score_curr[vi]);
+            score_curr[vi] = score_next[vi - g.start_vertex];
         }
         ///all reduce the local_diff value to global_diff
         double global_diff;
